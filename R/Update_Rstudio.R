77db8ed800997ce084ed1fa9c6774cadd85d0a6a
alphaprime7 <awesome.tingwei@outlook.com>
2023-11-20 13:19:11 -0600

    Initial commit

---
+++ R/Update_Rstudio.R
@@ -0,0 +1,14 @@
+# Using installr for R
+install.packages('installr')
+library(installr)
+updateR()
+
+# Updating R studio
+# 1.
+# Visit the Help > About RStudio > Check version
+# Navigate to https://posit.co/download/rstudio-desktop/; crosscheck version above to site version
+# if installed version is deprecated proceed below:
+
+#2.
+# Download the latest Rstudio version
+# Unless you are a cave man run the installation.
